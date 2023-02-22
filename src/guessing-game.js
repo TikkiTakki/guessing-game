f6e6198999f328a9f1d462821249706bcef1bd8a
TikkiTakki <115143189+TikkiTakki@users.noreply.github.com>
2023-02-22 06:59:22 +0300

    feat: add task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.result = Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.result;
     }
 
     greater() {
-
+        this.min = this.result;
     }
 }
 
